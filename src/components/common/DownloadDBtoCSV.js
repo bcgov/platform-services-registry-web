e7635ac9e8209a73f59e750a11c480c6a99bbb8f
Zhanna <kolezhannochka@gmail.com>
2022-10-12 11:32:19 -0700

    csv file downlading functionality

--- src/components/common/DownloadDBtoCSV.js
+++ src/components/common/DownloadDBtoCSV.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { gql, useLazyQuery } from "@apollo/client";
 import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
 import IconButton from '@mui/material/IconButton';
@@ -12,24 +12,22 @@ import Select from '@mui/material/Select';
 import Checkbox from '@mui/material/Checkbox';
 import { columns } from "../../pages/projects/helpers";
 import { Box } from "@mui/material";
-import { parse } from "json2csv";
+
 const DB_TO_CSV = gql`
   query privateCloudProjectsCSV(
-    $fields:[String], 
     $ministry: String, 
     $cluster: Int,
     $search: String,
      ) {
-        privateCloudProjectsCSV(
-        fields:$fields, 
+        privateCloudProjectsCSV(        
         ministry:$ministry, 
         cluster:$cluster,
         search:$search,
     ) {
-      csv    
+       
       projects {
         ... on PrivateCloudProject {
-        #   id
+          id
           name
           description
           cluster
@@ -51,71 +49,73 @@ const DB_TO_CSV = gql`
      }
 `;
 export default function DownloadDBtoCSV({
-    ministry,
-    cluster,
-    search,
+  ministry,
+  cluster,
+  search,
 }) {
+  const [fields, setFields] = useState([]);
 
-    const [fields, setFields] = useState([]);
-   
-    const handleChange = (event) => {
-        const {
-            target: { value },
-        } = event;
-        setFields(
-            typeof value === 'string' ? value.split(',') : value,
-        );
-    };
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setFields(
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
 
-    const [loadCollection, { data, error }] = useLazyQuery(DB_TO_CSV, {
-        variables: {
-            fields: fields,
-            ministry: ministry,
-            cluster: cluster,
-            search: search
-        },
-    });
-    if (error) return `Error! ${error.message}`;
-    data && createFile(data.privateCloudProjectsCSV.projects)
-    function createFile(fileData) {
-        let tmpArr = ''
-const fields = ['name', 'description', 'projectOwner','technicalLeads']
-fileData.map((item, index) => 
-Object.keys(item).map((key) =>{
-  if((Array.isArray(item[key]))){
-    item[key].map((lead) => {
-       if  (fields.indexOf(key)!==-1){
-       tmpArr+=`${lead.firstName} ${lead.lastName}' '`
-    }   
-      
+  const createCSVString = (fileData) => {
+    let subFields = `${Object.keys(fileData[0]).filter(field => fields.indexOf(field) !== -1).join(',')}\n`
+
+    fileData.map((item) => {
+      Object.keys(item).map((key) => {
+        if ((Array.isArray(item[key]))) {
+          item[key].map((lead) => {
+            if (fields.indexOf(key) !== -1) {
+              subFields += `${lead.firstName} ${lead.lastName} `
+            }
+          })
+          subFields += ','
+        }
+        else if (typeof (item[key]) !== 'object' && !Array.isArray(item[key])) {
+          if (fields.indexOf(key) !== -1) { subFields += `${item[key]},` }
+        }
+        else {
+          if (fields.indexOf(key) !== -1) {
+            subFields += `${item[key].firstName} ${item[key].lastName},`
+          }
+        }
+      })
+      subFields += '\n'
     })
+    return subFields
   }
- else if(typeof(item[key]) !== 'object'&&!Array.isArray(item[key])){
-  if  (fields.indexOf(key)!==-1) {tmpArr+=`${item[key]}','`}
-  } 
-  else{
-    if  (fields.indexOf(key)!==-1){
-       tmpArr+=`${item[key].firstName} ${item[key].lastName}','`
-    }    
-  }
-}
-)
-)
-        console.log(tmpArr)
-        let csvContent = "data:text/csv;charset=utf-8," + tmpArr;
-        let encodedUri = encodeURI(csvContent);
-        let link = document.createElement("a");
-        link.setAttribute("href", encodedUri);
-        link.setAttribute("download", "my_data.csv");
-        document.body.appendChild(link).click();
-    }
-  
 
-    return (
-        <Box sx={{ ml: "auto", display:'inline-flex', alignItems: 'center'}}>
-            <FormControl sx={{ m: 1, width: 300 }}>
-                <InputLabel>Fields</InputLabel>
-                <Select
+  const createFile = (fileData) => {
+    let csvContent = "data:text/csv;charset=utf-8," + createCSVString(fileData);
+    let encodedUri = encodeURI(csvContent);
+    let link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "my_data.csv");
+    document.body.appendChild(link).click();
+  }
+
+  const [loadCollection, { data, error }] = useLazyQuery(DB_TO_CSV, {
+    variables: {
+      ministry: ministry,
+      cluster: cluster,
+      search: search
+    },
+  });
+  if (error) return `Error! ${error.message}`;
+
+  data && createFile(data.privateCloudProjectsCSV.projects)
+
+  return (
+    <Box sx={{ ml: "auto", display: 'inline-flex', alignItems: 'center', p: 0 }}>
+      <FormControl sx={{ m: 1, width: 300 }}>
+        <InputLabel>Fields</InputLabel>
+        <Select
           labelId="demo-multiple-checkbox-label"
           id="demo-multiple-checkbox"
           multiple
@@ -126,16 +126,16 @@ Object.keys(item).map((key) =>{
         >
           {columns.map((column) => (
             <MenuItem key={column.id}
-            value={column.id}>
+              value={column.id}>
               <Checkbox checked={fields.indexOf(column.id) > -1} />
               <ListItemText primary={column.label} />
             </MenuItem>
           ))}
         </Select>
-            </FormControl>
-            <IconButton size="large" onClick={() => loadCollection()} disabled={!fields.length>0}>
-                <CloudDownloadIcon />
-            </IconButton>
-        </Box>
-    );
+      </FormControl>
+      <IconButton size="large" onClick={() => loadCollection()} disabled={!fields.length > 0}>
+        <CloudDownloadIcon />
+      </IconButton>
+    </Box>
+  );
 }
