3f53746c7492d70977b28d5c2ef7d254c74f7443
Oamar <okanji@Oamars-MacBook-Pro.local>
2023-03-01 02:43:25 -0800

    cashing ms graph api token in local storage

--- src/msGraphApi/config.js
+++ src/msGraphApi/config.js
@@ -11,9 +11,13 @@ export const msalConfig = {
     redirectUri: "/",
   },
   cache: {
-    cacheLocation: "sessionStorage", // This configures where your cache will be stored
-    storeAuthStateInCookie: false, // Set this to 'true' if you are having issues on IE11 or Edge
+    cacheLocation: "localStorage",
+    storeAuthStateInCookie: true,
   },
+  // cache: {
+  //   cacheLocation: "sessionStorage", // This configures where your cache will be stored
+  //   storeAuthStateInCookie: false, // Set this to 'true' if you are having issues on IE11 or Edge
+  // },
 };
 
 // Add scopes here for ID token to be used at Microsoft identity platform endpoints.
