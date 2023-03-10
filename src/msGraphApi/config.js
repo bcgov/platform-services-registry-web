bd642a6e8d902c4ac7a7b7e47f17cab187ec6798
okanji <oamarkanji@gmail.com>
2023-03-09 16:08:11 -0800

    Removed chaching of ms graph api token

--- src/msGraphApi/config.js
+++ src/msGraphApi/config.js
@@ -10,10 +10,10 @@ export const msalConfig = {
       "https://login.microsoftonline.com/6fdb5200-3d0d-4a8a-b036-d3685e359adc", // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
     redirectUri: "/",
   },
-  cache: {
-    cacheLocation: "localStorage",
-    storeAuthStateInCookie: true,
-  },
+  // cache: {
+  //   cacheLocation: "localStorage",
+  //   storeAuthStateInCookie: true,
+  // },
   // cache: {
   //   cacheLocation: "sessionStorage", // This configures where your cache will be stored
   //   storeAuthStateInCookie: false, // Set this to 'true' if you are having issues on IE11 or Edge
