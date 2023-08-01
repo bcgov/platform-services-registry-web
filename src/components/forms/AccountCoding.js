25afedcc4ba775485a75296918980551b893d6f0
unknown <kolezhannochka@gmail.com>
2023-08-01 11:24:49 -0700

    account coding field hot fixies

--- src/components/forms/AccountCoding.js
+++ src/components/forms/AccountCoding.js
@@ -6,7 +6,6 @@ import RequiredField from "../common/RequiredField";
 import Typography from "@mui/material/Typography";
 
 export default function AccountCoding({ formik, isDisabled }) {
-  const [value, setValue] = useState('');
   const [errorLengthMessage, setErrorLengthMessage] = useState('');
   const [errorSpecialCharMessage, setErrorSpecialCharMessage] = useState('');
   const [error, setError] = useState(false);
@@ -14,34 +13,6 @@ export default function AccountCoding({ formik, isDisabled }) {
   const hasSpecialCharacters = (str) => {
     const specialChars = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
     return specialChars.test(str);
-  }
-
-  useEffect(() => {
-    setValue(formik.values.accountCoding?.toUpperCase()
-      .replace(/[^A-Z0-9]/g, '')
-      .replace(/\s/g, '')
-      .replace(/(.{3})(.{5})(.{5})(.{4})(.{7})/g, "$1 $2 $3 $4 $5"))
-  }, [formik.values.accountCoding])
-
-  const handleChange = (e) => {
-    if (hasSpecialCharacters(e.target.value)) {
-      setErrorSpecialCharMessage("Wrong input only digits and Uppercase characters are allowed")
-    }
-    else if (e.target.value.replace(/[\s]/g, '').length !== 24) {
-      if (e.target.value.replace(/[\s]/g, '').length < 24) {
-        setErrorLengthMessage(`There are ${e.target.value.replace(/\s/g, '').length} characters - code should be 24 characters`)
-        setError(true)
-      }
-      if (e.target.value.replace(/[\s]/g, '').length > 24) {
-        setErrorLengthMessage(`There are more than 24 characters - code should be 24 characters`)
-      }
-    }
-    else {
-      setError(false)
-      setErrorLengthMessage('')
-      setErrorSpecialCharMessage('')
-    }
-    formik.handleChange(e);
   }
 
   return (
@@ -70,8 +41,12 @@ export default function AccountCoding({ formik, isDisabled }) {
             name="accountCoding"
             label="Account Coding"
             disabled={isDisabled}
-            value={value}
-            onChange={handleChange}
+            value={formik.values.accountCoding?.toUpperCase()
+              .replace(/[^A-Z0-9]/g, '')
+              .replace(/\s/g, '')
+              .replace(/(.{3})(.{5})(.{5})(.{4})(.{7})/g, "$1 $2 $3 $4 $5")}
+            onChange={formik.handleChange}
+            required
             placeholder="Enter account coding here (e.g. 000 22222 99898 6666 9898989)"
             error={
               (formik.touched.accountCoding && Boolean(formik.errors.accountCoding)) || error
@@ -81,9 +56,25 @@ export default function AccountCoding({ formik, isDisabled }) {
             }
             size="small"
             onInput={(e) => {
-              e.target.value = e.target.value.replace(/[\s]/g, '')
+              if (hasSpecialCharacters(e.target.value)) {
+                setErrorSpecialCharMessage("Wrong input only digits and Uppercase characters are allowed")
+              }
+              else if (e.target.value.length !== 24) {
+                if (e.target.value.length < 24) {
+                  setErrorLengthMessage(`There are ${e.target.value.replace(/\s/g, '').length} characters - code should be 24 characters`)
+                  setError(true)
+                }
+                if (e.target.value.length > 24) {
+                  setErrorLengthMessage(`There are more than 24 characters - code should be 24 characters`)
+                }
+              }
+              else {
+                setError(false)
+                setErrorLengthMessage('')
+                setErrorSpecialCharMessage('')
+              }
+              e.target.value = e.target.value.replace(/[@#$%^&*()_+[\]{}|\\,.?:;'"!<>~`\-=/\s]/g, '').toUpperCase().substring(0, 24)
             }}
-            inputProps={{ maxLength: 24 }}
           />
           <Typography sx={{ mb: 1 }} color="text.primary">
             Please make sure that the account coding entered above matches the account coding on the MoU for this project.                      If the account coding is changed at any point, all charges in the current quarter will be applied to the new account coding            </Typography>
