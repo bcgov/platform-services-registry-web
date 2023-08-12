c5ca3a5ce8606fa69775853204d90e26a0aeb483
funtigr <funtigr@gmail.com>
2023-08-12 01:23:52 -0700

    Plain text for first 4 fields for admin page

--- src/pages/project/privateCloud/AdminProject.js
+++ src/pages/project/privateCloud/AdminProject.js
@@ -40,6 +40,8 @@ import RolesContext from "../../../context/roles";
 import UserContext from "../../../context/user";
 import Delete from "../../../components/Delete";
 import ClusterInputText from "../../../components/plainText/ClusterInput";
+import MetaDataInputText from "../../../components/plainText/MetaDataInput";
+import {default as MinistryInputText} from "../../../components/plainText/MinistryInput";
 
 const ADMIN_PROJECT = gql`
   query PrivateCloudProjectById($projectId: ID!) {
@@ -227,9 +229,9 @@ export default function AdminProject({ requestsRoute }) {
   const readOnlyAdminIsAbleToEdit =
     userContext.email === data?.privateCloudProjectById.projectOwner.email ||
     userContext.email ===
-      data?.privateCloudProjectById?.primaryTechnicalLead?.email ||
+    data?.privateCloudProjectById?.primaryTechnicalLead?.email ||
     userContext.email ===
-      data?.privateCloudProjectById?.secondaryTechnicalLead?.email;
+    data?.privateCloudProjectById?.secondaryTechnicalLead?.email;
 
   const [
     privateCloudProjectEditRequest,
@@ -418,20 +420,24 @@ export default function AdminProject({ requestsRoute }) {
           />
         ) : null}
         <Container>
-          <MetaDataInput formik={formik} isDisabled={isDisabled} />
+          {isDisabled ? <MetaDataInputText name={data?.privateCloudProjectById?.name}
+            description={data?.privateCloudProjectById?.description}/> 
+            : <MetaDataInput formik={formik} isDisabled={isDisabled}/>}
           <Divider variant="middle" sx={{ mt: 1, mb: 1 }} />
+          {isDisabled ? <><MinistryInputText ministry={data?.privateCloudProjectById?.ministry} />
+          <Box sx={{ pt: 2 }}><ClusterInputText cluster={data?.privateCloudProjectById?.cluster} /></Box></> 
+            : <div style={{ display: "flex" }}>
+            <MinistryInput formik={formik} isDisabled={isDisabled} />
+            <Box sx={{ pt: 5 }}>
+              <ClusterInputText cluster={formik.values.cluster} />
+            </Box>
+          </div>}
+            
           <div>
-            <div style={{ display: "flex" }}>
-              <MinistryInput formik={formik} isDisabled={isDisabled} />
-              <Box sx={{ pt: 5 }}>
-                <ClusterInputText cluster={formik.values.cluster} />
-              </Box>
-            </div>
-            <Divider variant="middle" sx={{ mt: 1, mb: 1 }} />
+            <Divider variant="middle" sx={{ mt: 1, mb: 1 }}/>
             <Namespaces
               cluster={data?.privateCloudProjectById?.cluster}
-              licencePlate={data?.privateCloudProjectById?.licencePlate}
-            />
+              licencePlate={data?.privateCloudProjectById?.licencePlate}/>
             <Divider variant="middle" sx={{ mt: 1, mb: 1 }} />
             <Users formik={formik} isDisabled={isDisabled} />
             <Divider variant="middle" sx={{ mt: 1, mb: 1 }} />
@@ -443,8 +449,7 @@ export default function AdminProject({ requestsRoute }) {
                 type="submit"
                 sx={{ mr: 1, width: "170px" }}
                 variant="contained"
-                disabled={isDisabled || !formik.dirty}
-              >
+                disabled={isDisabled || !formik.dirty}>
                 Submit
               </Button>
             ) : null}
@@ -452,8 +457,7 @@ export default function AdminProject({ requestsRoute }) {
               open={open}
               onClose={handleClose}
               aria-labelledby="modal-modal-title"
-              aria-describedby="modal-modal-description"
-            >
+              aria-describedby="modal-modal-description">
               <Box sx={style}>
                 <Typography id="modal-modal-title" variant="h6" component="h2">
                   Please Confirm Your Request
@@ -464,8 +468,7 @@ export default function AdminProject({ requestsRoute }) {
                     onClick={submitForm}
                     disabled={!formik.dirty}
                     sx={{ mr: 1, width: "170px", mt: 3 }}
-                    variant="contained"
-                  >
+                    variant="contained">
                     Submit
                   </Button>
                 </Typography>
